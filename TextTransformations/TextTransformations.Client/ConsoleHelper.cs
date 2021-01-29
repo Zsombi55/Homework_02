43f0a79484f96c54738d6749642196905ac6faf3
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2021-01-29 11:12:15 +0200

    H2A2: remaking with more OOP, wip.

---
+++ TextTransformations/TextTransformations.Client/ConsoleHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace TextTransformations.Client
+{
+	public static class ConsoleHelper
+	{
+		/// <summary>
+		/// Asks the User for a single paragraph text input.
+		/// </summary>
+		/// <exception cref="System.ArgumentOutOfRangeException">Thrown when entered text exceeds the maximum number of characters, 254.</exception>
+		/// <exception cref="System.IO.IOException">Thrown when no text was entered OR only consists only of whitespace.</exception>
+		/// <returns>User input text.</returns>
+		public static string ReadText()
+		{
+			string s;
+			Console.WriteLine("------------\n");
+
+			try
+			{
+				s = Console.ReadLine();
+			}
+			catch(ArgumentOutOfRangeException e)
+			{
+				Console.WriteLine($"\tInput too long. Exceeded the maximum 254 character limit.\n{e}");
+				throw;
+			}
+
+			if(string.IsNullOrWhiteSpace(s))
+			{
+				throw new IOException("\tNo text input found.");
+			}
+
+			Console.WriteLine("------------\n");
+			return s;
+		}
+
+		/// <summary>
+		/// Takes a string then prints it to the standard output of the app.
+		/// </summary>
+		/// <param name="text">A string.</param>
+		public static void PrintText(string text)
+		{
+			Console.WriteLine(text);
+		}
+
+		/// <summary>
+		/// Takes in a text and selected manipulation data, modifies the text accordingly then returns the result.
+		/// </summary>
+		/// <param name="input">Text to manipulate.</param>
+		/// <param name="transformationRules">Manipulation data (conditions, rules, etc. ).</param>
+		/// <returns>Altered text.</returns>
+		public static string ApplyTransformationRules(
+            string input,
+            TransformationRule[] transformationRules)
+        {
+            if (transformationRules is null)
+            {
+                return input;
+            }
+
+            foreach (TransformationRule rule in transformationRules)
+            {
+                input = rule.Process(input);
+            }
+
+            return input;
+        }
+	}
+}
---
+++ TextTransformations/TextTransformations.Client/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * User: Zsombor
+ * Date: 2021-01-26
+ * Time: 10:40
+ * 2nd.
+ * --------------------------
+ * Revision Date: 2021-01-29
+ * Revision Time: 10:48
+ */
+
+using System;
+using TextTransformations.Former;
+
+namespace TextTransformations.Client
+{// Goal: Get text from User, perform string manipulations (replace a section with something else, remove a section, insert something to position, turn all cases upper & lower, make 2 linked transformations each using at least 2 variations) + Use OOP.
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter text, in one paragraph format, maximum 254 characters:");
+
+			string baseText = ConsoleHelper.ReadText();  //ConsoleHelper.PrintText(text);
+			
+            // Manipulation Batch 1
+			string result1 = ConsoleHelper.ApplyTransformationRules(baseText,
+                new TransformationRule[] 
+                {
+                    new InsertStringTransformationRule(0, "Petre "),
+                    new DeleteTextTransformationRule("test"),
+                    new ReplaceTextTransformationRule("a", "test123")
+                });
+
+            // Manipulation Batch 2
+            string result2 = ConsoleHelper.ApplyTransformationRules(baseText,
+                new TransformationRule[]
+                {
+                    new InsertStringTransformationRule(0, "Rodica "),
+                    new ReplaceTextTransformationRule("test", "test123")
+                });
+
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Modified text 1 is:");
+            Console.WriteLine(result1);
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Modified text 2 is:");
+            Console.WriteLine(result2);
+
+
+			Console.WriteLine("\nEnd.\n");
+		}
+	}
+}

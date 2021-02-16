a0e1cfa66681b726e140e2df95b1df050fba31a3
V. Zsombor <47913849+Zsombi55@users.noreply.github.com>
2021-02-16 20:18:26 +0200

    Deleted unused directory.

--- QuizSystem/QuizSystem.Library/Rules.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace QuizSystem.Validator
-{
-	public abstract class Rules
-	{
-		// This has to be super generic base class to be implemented later.
-		public abstract bool Validate(object input);
-	}
-}

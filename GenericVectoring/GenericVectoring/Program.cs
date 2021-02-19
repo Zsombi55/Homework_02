61cddeea1541f706afd3de733cddd3e596270330
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2021-02-19 21:54:07 +0200

    H2A5: beginning..

---
+++ GenericVectoring/GenericVectoring/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * User: Zsombor
+ * Date: 2021-02-19
+ * Time: 21:50
+ * 5th.
+ */
+
+using System;
+
+
+/// <summary>
+/// Sa se proiecteze o clasa generica, denumita ArrayHelper<T>, care sa implementeze urmatoarele servicii pentru lucrul cu vectori:
+///
+/// Returnarea indexului unui element dat (ca valoare):
+/// 	- Daca elementul nu este continut in tablou sa se returneze -1;
+/// 	- Testarea de egalitate sa se realizeze prin impunerea unei restriictii asupra tipului generic, anume ca acesta sa implementeze interfata IEquatable<T> ;
+///
+/// Returnarea unui sub-sir, pe baza unui index de start si a unei lungimi (cate elemente sa fie incluse in sub-sir):
+/// 	- Ordonarea sirului;
+/// 	- Pentru acesta puneti o restrictie asupra tipului generic T, astfel incat acesta sa implementeze interfata IComparable<T> .
+/// </summary>
+namespace GenericVectoring
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
